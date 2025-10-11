f6c118db0ba057234b82c86c2c3c867ecc2960c2
Nam Le <namle0017@gmail.com>
2025-10-11 21:43:50 +0700

    testing

---
+++ BE/VoltSwap.API/Controllers/StationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VoltSwap.BusinessLayer.Services;
+
+namespace VoltSwap.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationController : ControllerBase
+    {
+        private readonly StationService _stationService;
+        public StationController(StationService stationService)
+        {
+            _stationService = stationService;
+        }
+
+        [HttpGet("station-list")]
+        public async Task<IActionResult> StationList()
+        {
+            var getStationList = await _stationService.GetStationList();
+            return StatusCode(getStationList.Status, new
+            {
+                getStationList.Message,
+                getStationList.Data,
+            }); ;
+        }
+    }
+}
