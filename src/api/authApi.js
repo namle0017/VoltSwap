f39ae3e902d3af4f4dd5f2d8fadc4fa080dbe0cd
Khoa Phạm <doremonkhoa2005@gmail.com>
2025-10-30 11:14:13 +0700

    update

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base URL cho module Auth
 const AUTH_API = axios.create({
-  baseURL: "https://2472584bb4b5.ngrok-free.app/api/Auth", // thay bằng URL ngrok hiện tại của bạn
+  baseURL: "https://c5f4c7e90a71.ngrok-free.app/api/Auth", // thay bằng URL ngrok hiện tại của bạn
   headers: { "Content-Type": "application/json" },
 });
 
